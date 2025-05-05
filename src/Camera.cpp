7a25f18c02c44c0f4492419dcf20b1078784020e
Gonçalo Araújo <goncaloaraujo2004@gmail.com>
2025-05-05 18:03:21 +0100

    Cmakelists add automaticamente os files, organizaçao dos files

---
+++ src/Camera.cpp
@@ -0,0 +1,13 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glfw/glfw3.h>
+
+#include <iostream>
+#include <vector>
+
+class Camera {
+        public:
+                glm::vec3 position;
+                
+};
